549070547523477fab82e1ecd9e0e1a16ec4e510
liam <liam.l@binance.com>
2025-02-13 11:52:16 +0800

    wagmi

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 'use client'
-// import {RainbowProvider, ReownProvider} from "./providers";
-// import {ConnectButton} from '@rainbow-me/rainbowkit';
+import {RainbowProvider, ReownProvider} from "./providers";
+import {ConnectButton} from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
 
-// import {
-//   DynamicContextProvider,
-//   DynamicWidget,
-// } from "@dynamic-labs/sdk-react-core";
-// import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+} from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import {config} from "./connectors/wagmi";
 import {WagmiProvider} from "wagmi";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
@@ -17,28 +17,28 @@ const queryClient = new QueryClient();
 export default function Home() {
     return (
       <div className='flex flex-col gap-5'>
-        {/*<ReownProvider>*/}
-        {/*  Reown:*/}
-        {/*     /!*@ts-expect-error expected*!/*/}
-        {/*  <appkit-button/>*/}
-        {/*</ReownProvider>*/}
-        {/*<RainbowProvider>*/}
-        {/*  RainbowKit:*/}
-        {/*  <ConnectButton/>*/}
-        {/*</RainbowProvider>*/}
-        {/*<DynamicContextProvider*/}
-        {/*    settings={{*/}
-        {/*      // Find your environment id at https://app.dynamic.xyz/dashboard/developer*/}
-        {/*      environmentId: "df8b3d4c-bb1e-4c62-aa9a-c1cb71f0e605",*/}
-        {/*      walletConnectors: [EthereumWalletConnectors],*/}
-        {/*      recommendedWallets: [*/}
-        {/*        { walletKey: "binance", label: "Popular" },*/}
-        {/*      ],*/}
-        {/*    }}*/}
-        {/*>*/}
-        {/*  Dynamic xyz:*/}
-        {/*  <DynamicWidget />*/}
-        {/*</DynamicContextProvider>*/}
+        <ReownProvider>
+          Reown:
+             {/*@ts-expect-error expected*/}
+          <appkit-button/>
+        </ReownProvider>
+        <RainbowProvider>
+          RainbowKit:
+          <ConnectButton/>
+        </RainbowProvider>
+        <DynamicContextProvider
+            settings={{
+              // Find your environment id at https://app.dynamic.xyz/dashboard/developer
+              environmentId: "df8b3d4c-bb1e-4c62-aa9a-c1cb71f0e605",
+              walletConnectors: [EthereumWalletConnectors],
+              recommendedWallets: [
+                { walletKey: "binance", label: "Popular" },
+              ],
+            }}
+        >
+          Dynamic xyz:
+          <DynamicWidget />
+        </DynamicContextProvider>
         <WagmiProvider config={config}>
           <QueryClientProvider client={queryClient}>
             <div>
